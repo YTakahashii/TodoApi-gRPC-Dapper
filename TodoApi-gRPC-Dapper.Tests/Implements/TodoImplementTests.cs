8c687f25e5857d969256bfa9befa15a7668d7338
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-02-02 05:33:50 +0900

    :white_check_mark: Mockの設定
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

---
+++ TodoApi-gRPC-Dapper.Tests/Implements/TodoImplementTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+using Moq;
+using TodoApi_gRPC_Dapper.Models;
+using TodoApi_gRPC_Dapper.Models.Persistance;
+using TodoApi_gRPC_Dapper.Models.Repository;
+using TodoApi_gRPC_Dapper.Implements;
+using Proto.Todo;
+using Grpc.Core;
+using Grpc.Core.Utils;
+using Grpc.Core.Testing;
+using Google.Protobuf;
+
+namespace TodoApi_gRPC_Dapper.Tests.Implements
+{
+
+    public class TodoImplementTests
+    {
+        private readonly ITestOutputHelper outputHelper;
+        private List<Todo> todoItems;
+        private Mock<IUnitOfWork> uowMoq;
+        private Mock<ITodoItemRepository> todoItemRepoMoq;
+        private TodoImplement implement;
+
+        public TodoImplementTests(ITestOutputHelper outputHelper)
+        {
+            this.outputHelper = outputHelper;
+            todoItems = new List<Todo>()
+            {
+                new Todo{ Id = Guid.NewGuid().ToString("N"), Name = "Test1", IsComplete = false},
+                new Todo{ Id = Guid.NewGuid().ToString("N"), Name = "Test2", IsComplete = false},
+                new Todo{ Id = Guid.NewGuid().ToString("N"), Name = "Test3", IsComplete = false}
+            };
+            uowMoq = new Mock<IUnitOfWork>();
+            todoItemRepoMoq = new Mock<ITodoItemRepository>();
+            todoItemRepoMoq.Setup(x => x.FindAllAsync()).ReturnsAsync(todoItems);
+            todoItemRepoMoq.Setup(x => x.FindAsync(It.IsAny<String>()))
+                .ReturnsAsync((String id) => todoItems.Find(x => x.Id == id));
+            todoItemRepoMoq.SetupAsync(x => x.Add(It.IsAny<Todo>()))
+                .Callback<Todo>(item => todoItems.Add(item));
+            todoItemRepoMoq.SetupAsync(x => x.Update(It.IsAny<Todo>()))
+                .Callback<Todo>(item =>
+                {
+                    var index = todoItems.FindIndex(x => x.Id == item.Id);
+                    todoItems[index] = item;
+                });
+            todoItemRepoMoq.SetupAsync(x => x.Remove(It.IsAny<Todo>()))
+                .Callback<Todo>(item => todoItems.Remove(item));
+            todoItemRepoMoq.Setup(x => x.Count()).Returns(todoItems.Count);
+            uowMoq.Setup(x => x.TodoItems).Returns(todoItemRepoMoq.Object);
+
+            // controller = new TodoController(uowMoq.Object);
+            implement = new TodoImplement(uowMoq.Object);
+        }
+    }
+}

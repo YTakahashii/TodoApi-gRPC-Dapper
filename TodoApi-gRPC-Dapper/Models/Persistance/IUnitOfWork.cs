2281e00fa324944275d4bed89040b7d85d808f13
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-02-02 04:59:32 +0900

    :sparkles: UnitOfWork
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

---
+++ TodoApi-gRPC-Dapper/Models/Persistance/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using TodoApi_gRPC_Dapper.Models.Repository;
+
+namespace TodoApi_gRPC_Dapper.Models.Persistance
+{
+    public interface IUnitOfWork
+    {
+        ITodoItemRepository TodoItems { get; }
+    }
+}
---
+++ TodoApi-gRPC-Dapper/Models/Persistance/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TodoApi_gRPC_Dapper.Models.Repository;
+
+namespace TodoApi_gRPC_Dapper.Models.Persistance
+{
+    public class UnitOfWork
+    {
+        private ITodoItemRepository todoItems;
+
+        public UnitOfWork()
+        {
+        }
+
+        public ITodoItemRepository TodoItems
+        {
+            get
+            {
+                if (todoItems == null)
+                {
+                    todoItems = new TodoItemRepository();
+                }
+
+                return todoItems;
+            }
+        }
+    }
+}

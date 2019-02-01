bdb23a953687ba86630758e5686644cd8d796608
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-02-02 05:26:51 +0900

    :sparkles: 5000番ポートで起動するように設定
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

--- TodoApi-gRPC-Dapper/Program.cs
+++ TodoApi-gRPC-Dapper/Program.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using TodoApi_gRPC_Dapper.Implements;
+using TodoApi_gRPC_Dapper.Models.Persistance;
+using Grpc;
+using Grpc.Core;
+using Proto.Todo;
 
 namespace TodoApi_gRPC_Dapper
 {
@@ -14,7 +19,24 @@ namespace TodoApi_gRPC_Dapper
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            const int PORT = 5000;
+            var todoImplement = new TodoImplement(new UnitOfWork());
+            var server = new Server
+            {
+                Services =
+                {
+                    TodoService.BindService(todoImplement)
+                },
+                Ports = { new ServerPort("0.0.0.0", PORT, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("gRPC Todo server listening on port " + PORT);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.Read();
+
+            server.ShutdownAsync().Wait();
+            // CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>

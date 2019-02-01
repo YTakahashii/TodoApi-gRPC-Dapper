7bd7919ed4c70b5b26ccbb229e81da5093c2dcaf
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-02-02 04:56:10 +0900

    :sparkles: SqlHandler
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

---
+++ TodoApi-gRPC-Dapper/Models/SqlHandler/SqlHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace TodoApi_gRPC_Dapper.Models.SqlHandler
+{
+    public class SqlHandler
+    {
+        public SqlHandler()
+        {
+        }
+
+        public SqlConnection GetConnection()
+        {
+            var connectionString = "Data Source=127.0.0.1;Initial Catalog=TodoApi_Dapper;Connect Timeout=60;Persist Security Info=True;User ID=sa;Password=Password0123";
+            var connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}

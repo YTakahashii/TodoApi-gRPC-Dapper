3e093ac08c95274f12620de60fd223a00fccc8a0
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-02-02 16:37:06 +0900

    :sparkles: PostTodoItem Serviceの実装　:shirt: コードスタイルの修正
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

--- TodoApi-gRPC-Dapper/Implements/TodoImplement.cs
+++ TodoApi-gRPC-Dapper/Implements/TodoImplement.cs
@@ -3,41 +3,41 @@ using System.Threading.Tasks;
 using Grpc;
 using Grpc.Core;
 using Grpc.Core.Utils;
+using Proto.Todo;
 using TodoApi_gRPC_Dapper.Models;
 using TodoApi_gRPC_Dapper.Models.Persistance;
-using Proto.Todo;
 
-namespace TodoApi_gRPC_Dapper.Implements
-{
-    public class TodoImplement: TodoService.TodoServiceBase
-    {
+namespace TodoApi_gRPC_Dapper.Implements {
+    public class TodoImplement : TodoService.TodoServiceBase {
         private IUnitOfWork _unitOfWork;
 
-        public TodoImplement(IUnitOfWork unitOfWork): base()
-        {
+        public TodoImplement (IUnitOfWork unitOfWork) : base () {
             _unitOfWork = unitOfWork;
-            if (_unitOfWork.TodoItems.Count() == 0)
-            {
-                _unitOfWork.TodoItems.Add(new Todo { Name = "Item1", IsComplete = false });
+            if (_unitOfWork.TodoItems.Count () == 0) {
+                _unitOfWork.TodoItems.Add (new Todo { Name = "Item1", IsComplete = false });
             }
         }
 
-        public override async Task<GetTodoItemsResponse> GetTodoItems(Empty request, ServerCallContext context)
-        {
-            var result = await _unitOfWork.TodoItems.FindAllAsync();
-            var response = new GetTodoItemsResponse();
-            foreach (var todo in result)
-            {
-                response.Todos.Add(todo);
+        public override async Task<GetTodoItemsResponse> GetTodoItems (Empty request, ServerCallContext context) {
+            var result = await _unitOfWork.TodoItems.FindAllAsync ();
+            var response = new GetTodoItemsResponse ();
+            foreach (var todo in result) {
+                response.Todos.Add (todo);
             }
 
             return response;
         }
 
-        public override async Task<GetTodoItemResponse> GetTodoItem(GetTodoItemRequest request, ServerCallContext context)
-        {
-            var todo = await _unitOfWork.TodoItems.FindAsync(request.Id);
+        public override async Task<GetTodoItemResponse> GetTodoItem (GetTodoItemRequest request, ServerCallContext context) {
+            var todo = await _unitOfWork.TodoItems.FindAsync (request.Id);
             var response = new GetTodoItemResponse { Todo = todo };
+
+            return response;
+        }
+
+        public override async Task<PostTodoItemResponse> PostTodoItem (PostTodoItemRequest request, ServerCallContext context) {
+            var addedTodoItem = await _unitOfWork.TodoItems.Add (new Todo { Name = request.Name });
+            var response = new PostTodoItemResponse { Todo = addedTodoItem };
 
             return response;
         }
